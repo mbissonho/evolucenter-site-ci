18bdd535e304e864d828539658b0243c47a98288
Mateus A. Bissonho <mateus_bissonho@live.com>
2020-03-08 23:34:53 -0300

    Refatorando arquivos de estilo: melhorada organização e reuso de variáveis no arquivo footer.scss

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,9 +2,9 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
     <link rel="stylesheet" href="<?= base_url('assets/css/style.min.css'); ?>">
 
